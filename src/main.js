dc943164af2478ac70813cf74f704c1fa7fc153c
ile2525 <2405565@bdeb.qc.ca>
2026-03-05 12:23:14 -0500

    Premier_Depot

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { dessinSymetrique } from './untils/dessinSymetrique.js';
+
+import './components/layouts/header.css';
+import './components/buttons/button.css';
+import './components/layouts/footer.css';
+
+import './styles/accueil.css';
+import './styles/projets.css';
+import './styles/aPropos.css';
+
+import './style.css';
+
